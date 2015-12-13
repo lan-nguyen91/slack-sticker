ecf64c42fd9e83c36cb4218dc64532891e4f0dee
Lan Nguyen <lan.nguyen@hart.com>
2015-12-12 16:41:46 -0800

    upload fix

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ let koa        = require('koa');
 let router     = require('koa-router');
 let logger     = require('koa-logger');
 let bodyParser = require('koa-bodyparser');
-let serve = require('koa-static');
-let parse = require('co-busboy');
-let fs    = require('fs');
-let path  = require('path');
+let serve      = require('koa-static');
+let parse      = require('co-busboy');
+let fs         = require('fs');
+let path       = require('path');
+let tfy        = require('thenify');
 
 
 let app        = new koa();
@@ -17,21 +18,29 @@ app.use(serve(__dirname + '/public'));
 
 let sticker    = require('./controllers/sticker');
 let stickerRouter = new router()
+const uploadPath = './public/uploads/';
 
 app.use(function *(next){
   // ignore non-POSTs
   if ('POST' != this.method) return yield next;
 
-  if(this.path == '/upload') { 
+  if(this.path == '/upload') {
     let parts = parse(this);
     let part;
 
     while (part = yield parts) {
-      let query_split = part.fieldname.split('-'); 
-      let folder      = query_split[0]; 
+      let query_split = part.fieldname.split('-');
+
+      if (query_split.length != 2) this.body("field name is invalid");
+      let folder      = query_split[0];
       let stickerName = query_split[1];
 
-      let stream = fs.createWriteStream('./uploads/' + folder + '/' + stickerName + path.extname(part.filename));
+      // if directory not exist create one
+      if (!fs.existsSync(uploadPath + folder)) {
+        fs.mkdirSync(uploadPath + folder);
+      }
+
+      let stream = fs.createWriteStream(uploadPath + folder + '/' + stickerName + path.extname(part.filename));
       part.pipe(stream);
       this.body = "uploaded"
     }
